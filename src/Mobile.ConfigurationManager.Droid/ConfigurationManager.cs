0fb72cd28b0ba943b2276d47efcce2039c3419ce
Chase Florell <chase.florell@gmail.com>
2019-04-23 21:23:15 -0700

    [CF] Alternate way to initialize Android

--- src/Mobile.ConfigurationManager.Droid/ConfigurationManager.cs
+++ src/Mobile.ConfigurationManager.Droid/ConfigurationManager.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using Android.Content;
 
 namespace Mobile.ConfigurationManager
 {
@@ -7,6 +8,12 @@ namespace Mobile.ConfigurationManager
         public static void Init(string config = "App.config")
         {
             using (var stream = new StreamReader(Android.App.Application.Context.Assets.Open(config)))
+                InitInternal(stream);
+        }
+
+        public static void Init(Context context, string config = "App.config")
+        {
+            using (var stream = new StreamReader(context.Assets.Open(config)))
                 InitInternal(stream);
         }
     }
